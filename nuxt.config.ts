6266e5fa175084019ecabcc9429356682d47c887
Carlos Davi <carlos@datasource.expert>
2024-07-29 22:42:59 -0300

    Build: Adicionado dependencia - NuxtImage

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,5 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@pinia/nuxt', "@nuxtjs/tailwindcss", "@nuxtjs/i18n"],
+  modules: ['@pinia/nuxt', "@nuxtjs/tailwindcss", "@nuxtjs/i18n", "@nuxt/image"],
 })
